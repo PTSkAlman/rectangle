10e76744b180b980e7d847a17acf825fdc943e27
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2021-09-13 10:00:50 +0200

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,51 @@
+public class Rectangle {
+
+    private float length;
+    private float width;
+    private double area;
+    private double perimeter;
+
+    public Rectangle() {
+        length = 1.0f;
+        width = 1.0f;
+    }
+
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public double getArea() {
+        this.area = width*length;
+        return area;
+    }
+
+    public double getPerimeter() {
+        this.perimeter = 2*length + 2*width;
+        return perimeter;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+}
